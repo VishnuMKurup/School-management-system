f7e28ce5285c3d4dbdeded4ad2cf281d0ef01b2f
VishnuMKurup <vishnujishnu077@gmail.com>
2022-08-04 15:34:27 +0530

    3 files modified

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,7 +2,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { AuthService } from '../auth.service';
-import { School } from '../school';
 
 @Component({
   selector: 'app-login-component',
